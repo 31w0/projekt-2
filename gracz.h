69d575abd0bb98973beeb66d83a3b6155c395c8b
Marcel Maślak <201962400+31w0@users.noreply.github.com>
2026-05-11 06:21:28 +0200

    Update gracz.h

--- gracz.h
+++ gracz.h
@@ -1,25 +1,23 @@
 #pragma once
 #include "Karta.h"
 
-// Deklaracja wyprzedzająca (forward declaration) w celu uniknięcia pętli załączeń
 class Kasyno; 
 
 class Gracz {
-private:
+
+protected: 
     Karta* reka[10];
     int liczbaKart;
     int sumaPunktow;
-    bool spasowal; // Informacja czy gracz spasował
+    bool spasowal;
 
 public:
     Gracz();
     void wezKarte(Karta* _karta);
     void pokazKarty();
     int getSumaPunktow() const { return sumaPunktow; }
-    
     bool getSpasowal() const { return spasowal; } 
     
     void decyduj(Kasyno* kasyno); 
-
-    void resetuj(); // Resetuje stan gracza przed nowa gra
+    void resetuj();
 };
