0511da4301d5b09e44d870cb4c1ac839ee6fa27c
Marcel Maślak <201962400+31w0@users.noreply.github.com>
2026-06-08 06:05:27 +0200

    Expand main function to support Dwustawny and PID controllers
    Refactor main function to include two controllers: Dwustawny and PID. Update output filenames for results.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "Symulacja.h"
+#include "Dwustawny.h"
+#include "PID.h"
+#include <iostream>
 
 int main() {
-    Symulacja symulacja;
+    std::cout << " SYMULACJA: REGULATOR DWUSTAWNY \n";
+    Symulacja sym1;
+    Dwustawny regDwustawny(sym1.getPomieszczenie(), sym1.getGrzejnik(), 22.0f);
+    sym1.ustawRegulator(&regDwustawny);
+    sym1.przebieg(300, 1.0f);
+    sym1.zapiszWyniki("wyniki_dwustawny.csv");
 
-    symulacja.przebieg(100, 10.0f);
-
-    symulacja.zapiszWyniki("wyniki.csv");
+    std::cout << "\n--- SYMULACJA: REGULATOR PID ---\n";
+    Symulacja sym2;
+    PID regPID(sym2.getPomieszczenie(), sym2.getGrzejnik(), 22.0f);
+    sym2.ustawRegulator(&regPID);
+    sym2.przebieg(300, 1.0f);
+    sym2.zapiszWyniki("wyniki_pid.csv");
 
     return 0;
 }
