2511d4685a9f5b13340265eb1829723675c98a6e
Marcel Maślak <201962400+31w0@users.noreply.github.com>
2026-06-08 06:04:32 +0200

    Update Symulacja.h

--- Symulacja.h
+++ Symulacja.h
@@ -4,20 +4,28 @@
 #include <vector>
 #include <string>
 
+class Regulator;
+
 class Symulacja {
 private:
     Pomieszczenie pokoj;
     Grzejnik piec;
+    Regulator* regulator;
 
     std::vector<float> historiaCzasu;
     std::vector<float> historiaTemperatury;
+    std::vector<float> historiaGrzejnika;
     float czasSymulacji;
 
     void iteracja(float dT);
 
 public:
     Symulacja();
+
+    void ustawRegulator(Regulator* r) { regulator = r; }
+    Pomieszczenie* getPomieszczenie() { return &pokoj; }
+    Grzejnik* getGrzejnik() { return &piec; }
+
     void przebieg(int ileIteracji, float dT);
     void zapiszWyniki(const std::string& nazwaPliku);
 };
-
