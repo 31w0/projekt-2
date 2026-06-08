40720ffb4d9a0d9886ba241902098a5dd3762bbe
Marcel Maślak <201962400+31w0@users.noreply.github.com>
2026-06-08 06:04:48 +0200

    Update Symulacja.cpp

--- Symulacja.cpp
+++ Symulacja.cpp
@@ -1,28 +1,27 @@
 #include "Symulacja.h"
+#include "Regulator.h"
 #include <iostream>
 #include <fstream>
 
-
 Symulacja::Symulacja()
-    : pokoj(3.0f, 4.0f, 5.0f),
-    piec(5000.0f),
-    czasSymulacji(0.0f) {
-}
+    : pokoj(3.0f, 4.0f, 5.0f), piec(1000.0f), czasSymulacji(0.0f), regulator(nullptr) {}
 
 void Symulacja::iteracja(float dT) {
+    if (regulator != nullptr) {
+        regulator->aktualizuj(dT);
+    }
 
     float cieplo = piec.emitujCieplo(dT);
-
     pokoj.dodajCieplo(cieplo);
-
     pokoj.aktualizuj(dT);
-
     czasSymulacji += dT;
 
     historiaCzasu.push_back(czasSymulacji);
     historiaTemperatury.push_back(pokoj.getTemperatura());
+    historiaGrzejnika.push_back(piec.getNastawa());
 
-    std::cout << "Czas: " << czasSymulacji << "s | Temp: " << pokoj.getTemperatura() << " C\n";
+    std::cout << "Czas: " << czasSymulacji << "s | Temp: " << pokoj.getTemperatura() 
+              << " C | Nastawa: " << piec.getNastawa() << "\n";
 }
 
 void Symulacja::przebieg(int ileIteracji, float dT) {
@@ -34,11 +33,12 @@ void Symulacja::przebieg(int ileIteracji, float dT) {
 void Symulacja::zapiszWyniki(const std::string& nazwaPliku) {
     std::ofstream plik(nazwaPliku);
     if (plik.is_open()) {
-        plik << "Iteracja;Czas;Temperatura\n";
+        plik << "Iteracja;Czas;Temperatura;Nastawa\n";
         for (size_t i = 0; i < historiaCzasu.size(); ++i) {
-            plik << i << ";" << historiaCzasu[i] << ";" << historiaTemperatury[i] << "\n";
+            plik << i << ";" << historiaCzasu[i] << ";" 
+                 << historiaTemperatury[i] << ";" << historiaGrzejnika[i] << "\n";
         }
         plik.close();
-        std::cout << "\n Zapisano wyniki do pliku: " << nazwaPliku << "\n";
+        std::cout << "\n[Symulacja] Zapisano wyniki do pliku: " << nazwaPliku << "\n";
     }
 }
